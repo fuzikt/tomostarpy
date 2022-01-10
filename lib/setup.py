1a959cba834e234d6530b13abc3a8e39c1a75a8e
fuzikt <tibor.fuzik@ceitec.muni.cz>
2022-01-10 15:27:39 +0100

    make compatible with some systems

--- lib/setup.py
+++ lib/setup.py
@@ -1,11 +1,13 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
+import numpy
 
 ext_modules = [
     Extension(
         "placeback_subvolume_cyt",
         ["placeback_subvolume_cyt.pyx"],
         libraries=["m"],
+        include_dirs=[numpy.get_include()],
         extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ],
         extra_link_args=['-fopenmp']
     )
@@ -13,5 +15,6 @@ ext_modules = [
 
 setup(
     name='placeback_subvolume_cyt',
+    include_dirs=[numpy.get_include()],
     ext_modules=cythonize(ext_modules),
 )
