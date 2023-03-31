2c5e3390aa5db4de2aeaf8925edd6668c55b8111
fuzikt <tibor.fuzik@ceitec.muni.cz>
2023-03-31 10:53:33 +0200

    add polygon feature

--- lib/chimbild.py
+++ lib/chimbild.py
@@ -12,6 +12,16 @@ class bildArrowItem:
         self.shiftZ = shiftZ
         self.value = value
 
+class bildPolygonItem:
+    def __init__(self, polygonVertices, value=0, shiftX=0, shiftY=0, shiftZ=0, r=1, g=1, b=0):
+        self.polygonVertices = polygonVertices
+        self.r = r
+        self.g = g
+        self.b = b
+        self.shiftX = shiftX
+        self.shiftY = shiftY
+        self.shiftZ = shiftZ
+        self.value = value
 
 class bildArrowData:
 
@@ -55,3 +65,47 @@ class bildArrowData:
                 bildArrowFile.write(".arrow %0.3f %0.3f %0.3f %0.3f %0.3f %0.3f %0.3f\n" % (
                 item.shiftX, item.shiftY, item.shiftZ, item.x + item.shiftX, item.y + item.shiftY, item.z + item.shiftZ,
                 item.thickness))
+
+class bildPolygonData:
+
+    def __init__(self):
+        self.bildPolygonItems = []
+
+    def addItem(self, polygonVertices, value=0, shiftX=0, shiftY=0, shiftZ=0, r=1, g=1, b=0):
+        self.bildPolygonItems.append(bildPolygonItem(polygonVertices, value, shiftX, shiftY, shiftZ, r, g, b))
+
+    def createColorGradient(self):
+        rainbowArray = []
+        rangeColor = 512
+        for i in range(int(rangeColor / 2)):  # from red -> yellow
+            rainbowArray.append([1, i / (rangeColor / 2), 0])
+        for i in range(int(rangeColor / 2), 0, -1):  # from yellow -> green
+            rainbowArray.append([i / (rangeColor / 2), 1, 0])
+
+        # get the min and max value (ie range) of the desired label in the star file
+        minColor = self.bildPolygonItems[0].value
+        maxColor = self.bildPolygonItems[0].value
+        for item in self.bildPolygonItems:
+            minColor = min(minColor, item.value)
+            maxColor = max(maxColor, item.value)
+
+        for item in self.bildPolygonItems:
+            if minColor != maxColor:
+                coloringArrayElement = rainbowArray[
+                    int((item.value - minColor) / (maxColor - minColor) * (rangeColor - 1))]
+            else:
+                coloringArrayElement = [1, 1, 0]
+            self.bildPolygonItems[self.bildPolygonItems.index(item)].r = coloringArrayElement[0]
+            self.bildPolygonItems[self.bildPolygonItems.index(item)].g = coloringArrayElement[1]
+            self.bildPolygonItems[self.bildPolygonItems.index(item)].b = coloringArrayElement[2]
+
+    def writeBuildPolygonFile(self, outputBuildPolygonFile):
+        self.createColorGradient()
+
+        with open(outputBuildPolygonFile, 'w') as bildPolygonFile:
+            for item in self.bildPolygonItems:
+                bildPolygonFile.write(".color %0.3f %0.3f %0.3f\n" % (item.r, item.g, item.b))
+                bildPolygonFile.write(".polygon ")
+                for vertex in item.polygonVertices:
+                    bildPolygonFile.write("%0.3f %0.3f %0.3f " % (vertex[0]+item.shiftX,vertex[1]+item.shiftY,vertex[2]+item.shiftZ))
+                bildPolygonFile.write("\n")
